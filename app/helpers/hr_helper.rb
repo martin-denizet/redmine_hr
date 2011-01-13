c77a4257a9f1308703ddea1195c5a5231c7db5c2
Martin Denizet <martin.denizet@supinfo.com>
2011-01-14 02:02:59 +0800

    Fixed display according to permissions. Added a table to view for the positions.

--- app/helpers/hr_helper.rb
+++ app/helpers/hr_helper.rb
@@ -4,4 +4,8 @@ module HrHelper
      link_to l(:label_hr), url_for(:controller => 'hr', :action=>'index')
   end
 
+  def authorized_globally(controller,action)
+    User.current.allowed_to?({:controller => controller, :action => action},nil, :global => true)
+  end
+
 end
