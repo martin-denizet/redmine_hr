5f0eae17bda2f753bff585c5753212e0aca09119
Martin Denizet <martin.denizet@supinfo.com>
2011-01-20 00:05:45 +0800

    Edition feature for the employee list. Modifications of the colors of the Organization Chart. Improvement in display layouts.

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,30 @@
+class EmployeesController < ApplicationController
+  unloadable
+
+  helper :hr
+
+  helper :custom_fields
+
+  before_filter :authorize_global
+
+  def index
+    @user_positions =  UserPosition.find(:all,:order => "position_id")
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @user_details = UserDetails.find(:first, :conditions => ["user_id=?", @user.id])
+    if @user_details==nil
+      @user_details=UserDetails.new
+      @user_details.user_id=@user.id
+      @user_details.save
+    end
+
+    if request.post? and @user.update_attributes(params[:user_custom_fields]) and @user_details.update_attributes(params[:user_details])
+      if @user.save and @user_details.save
+        flash[:notice] = l(:notice_successful_update)
+      end
+    end
+
+  end
+end
--- app/helpers/positions_helper.rb
+++ app/helpers/positions_helper.rb
@@ -30,10 +30,9 @@ module PositionsHelper
     properties=[
       #      {:selected => {:from => '#FFDFDF', :to => '#FFBBBB', :border => '#FF9797'}, :style => {:from => '#FF9797', :to => '#FF7575',:border => '#FF4848'}},
 
-      {:style => {:'from' => '#D6F8DE', :'to' => '#E3FBE9',:'border' => '#93EEAA'}},
+      
       {:style => {:'from' => '#DBEADC', :'to' => '#E9F1EA',:'border' => '#A6CAA9'}},
       {:style => {:'from' => '#DFFFCA', :'to' => '#E8FFD9',:'border' => '#ABFF73'}},
-      {:style => {:'from' => '#FFFFC8', :'to' => '#FFFFD7',:'border' => '#FFFF84'}},
       {:style => {:'from' => '#CACAFF', :'to' => '#E1E1FF',:'border' => '#9999FF'}},
       {:style => {:'from' => '#D0E6FF', :'to' => '#DBEBFF',:'border' => '#99C7FF'}},
       {:style => {:'from' => '#D9F3FF', :'to' => '#ECFAFF',:'border' => '#A8E4FF'}},
@@ -42,7 +41,27 @@ module PositionsHelper
       {:style => {:'from' => '#D7D1F8', :'to' => '#E3E0FA',:'border' => '#A095EE'}},
       {:style => {:'from' => '#B8E2EF', :'to' => '#C9EAF3',:'border' => '#57BCD9'}},
       {:style => {:'from' => '#FFC8F2', :'to' => '#FFDFF8',:'border' => '#FFACEC'}},
-      {:style => {:'from' => '#DDCEFF', :'to' => '#E6DBFF',:'border' => '#C4ABFE'}}
+      {:style => {:'from' => '#DDCEFF', :'to' => '#E6DBFF',:'border' => '#C4ABFE'}},
+      {:style => {:'from' => '#FFFFC8', :'to' => '#FFFFD7',:'border' => '#FFFF84'}},
+      {:style => {:'from' => '#D6F8DE', :'to' => '#E3FBE9',:'border' => '#93EEAA'}},
+
+
+
+
+      #Copy of the lines above
+      {:style => {:'from' => '#DBEADC', :'to' => '#E9F1EA',:'border' => '#A6CAA9'}},
+      {:style => {:'from' => '#DFFFCA', :'to' => '#E8FFD9',:'border' => '#ABFF73'}},
+      {:style => {:'from' => '#CACAFF', :'to' => '#E1E1FF',:'border' => '#9999FF'}},
+      {:style => {:'from' => '#D0E6FF', :'to' => '#DBEBFF',:'border' => '#99C7FF'}},
+      {:style => {:'from' => '#D9F3FF', :'to' => '#ECFAFF',:'border' => '#A8E4FF'}},
+      {:style => {:'from' => '#CAFFD8', :'to' => '#EAFFEF',:'border' => '#8BFEA8'}},
+      {:style => {:'from' => '#E8C6FF', :'to' => '#EFD7FF',:'border' => '#C269FE'}},
+      {:style => {:'from' => '#D7D1F8', :'to' => '#E3E0FA',:'border' => '#A095EE'}},
+      {:style => {:'from' => '#B8E2EF', :'to' => '#C9EAF3',:'border' => '#57BCD9'}},
+      {:style => {:'from' => '#FFC8F2', :'to' => '#FFDFF8',:'border' => '#FFACEC'}},
+      {:style => {:'from' => '#DDCEFF', :'to' => '#E6DBFF',:'border' => '#C4ABFE'}},
+      {:style => {:'from' => '#FFFFC8', :'to' => '#FFFFD7',:'border' => '#FFFF84'}},
+      {:style => {:'from' => '#D6F8DE', :'to' => '#E3FBE9',:'border' => '#93EEAA'}}
       
     ]
 
